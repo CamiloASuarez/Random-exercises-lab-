0e8120672f9ab51a483211f1d58ead609c8f7c5d
Camilo A. Suarez <camiloandressuarez1@gmail.com>
2020-05-19 14:31:26 -0500

    Challenge 4

---
+++ ch4.c
@@ -0,0 +1,12 @@
+int main(){
+    char animal;
+
+    printf("Write your favorite animal: \n");
+    scanf("%c", &animal);
+
+    if (animal = strcmp("turtle","TURTLE","Turtle")){
+        printf("AWESOME, I like the turtles too!!!!");
+    }else if ( animal != 'turtle' ){
+        printf("OH!, Nice, but Turtles are better");
+    }
+}
